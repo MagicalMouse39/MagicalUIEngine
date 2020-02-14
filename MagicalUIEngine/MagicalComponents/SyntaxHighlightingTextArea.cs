b621c200489ae1b80ecf7c2ae4e6fc279b83553b
MagicalMouse39 <31729116+MagicalMouse39@users.noreply.github.com>
2020-02-14 23:35:03 +0100

    SyntaxHighlightningTextArea Added

--- MagicalUIEngine/MagicalComponents/SyntaxHighlightingTextArea.cs
+++ MagicalUIEngine/MagicalComponents/SyntaxHighlightingTextArea.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MagicalUIEngine.MagicalComponents
 {
-    class SyntaxHighlightingTextArea
+    class SyntaxHighlightingTextArea : RichTextBox
     {
+        public SyntaxHighlightingTextArea()
+        {
+            this.AcceptsTab = true;
+            this.Multiline = true;
+        }
     }
 }
